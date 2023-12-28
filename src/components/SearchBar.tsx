bd087a323e59ca70ba0d7e51a444892a066ea965
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-28 19:53:40 +0200

    add

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,12 @@
+import search from "../assets/react.svg";
+
 export const SearchBar = () => {
   return (
     <>
       <div className="Searchbar">
         <form className="SearchForm">
           <input type="text" className="SearchForm-input" />
-          <button className="SearchForm-button"></button>
+          <button className="SearchForm-button">{search}</button>
         </form>
       </div>
     </>
