b01496e3891fce9cfa90481aec05de08dc816443
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-28 07:43:44 +0200

    start task_3

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import user from "./data/user.json";
-import {Profile} from './components/Profile/Profile'
+// import user from "./data/user.json";
+
 
 function App() {
   return (
     <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
     </>
   );
 }
