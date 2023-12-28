4d6bb8316d399e042379f2e08ce9193821235f7e
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-28 16:15:39 +0200

    add localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Contact } from "./types";
 import { ContactForm } from "./components/ContactForm/ContactForm";
@@ -7,8 +7,16 @@ import { ContactsFilter } from "./components/ContactsFilter/ContactsFilter";
 import defaultContacts from "./data/contacts.json";
 
 function App() {
-  const [contacts, SetContacts] = useState<Contact[]>(defaultContacts);
-  const [filter, setFilter] = useState('');
+  const [contacts, SetContacts] = useState<Contact[]>(() => {
+    const storedContacts = JSON.parse(localStorage.getItem("contacts") as string);
+    return storedContacts || defaultContacts;
+  });
+  
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleOnSubmit = (contact: Contact) => {
     SetContacts((prev) => [...prev, contact]);
@@ -18,22 +26,19 @@ function App() {
     setFilter(e.target.value);
   };
 
-  const visibleContacts = contacts.filter(
-    (contact) => contact.name.toLowerCase().includes(filter.toLowerCase())
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   const handleOnDelete = (id: string) => {
-    SetContacts(visibleContacts.filter(contact => contact.id !== id))
-  }
-  console.log(visibleContacts)
+    SetContacts(visibleContacts.filter((contact) => contact.id !== id));
+  };
 
   return (
     <>
       <h1>Phone book</h1>
-      <ContactForm onSubmit={handleOnSubmit} contacts={contacts}/>
-      <ContactsFilter
-        handleOnInput={handleOnInput}
-      />
+      <ContactForm onSubmit={handleOnSubmit} contacts={contacts} />
+      <ContactsFilter handleOnInput={handleOnInput} />
       <ContactLIst onDelete={handleOnDelete} contacts={visibleContacts} />
     </>
   );
