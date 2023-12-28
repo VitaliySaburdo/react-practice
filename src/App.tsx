c367b9941170af0442f074c89ebe1733d5cf4aad
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-28 19:27:34 +0200

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { fetchData } from "../service/service";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await fetchData();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <h1>App</h1>
