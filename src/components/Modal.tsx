ff8e6e0673ed64f0ca23f98e920cd0c6225e2b87
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-30 22:45:55 +0200

    add modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,47 @@
-export const Modal = () => {
-  return <></>;
+import { useEffect } from "react";
+import cross from "../assets/cross.png";
+
+type ModalProps = {
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+export const Modal: React.FC<ModalProps> = ({ onClick, children }) => {
+  useEffect(() => {
+    const handleEscDown = (e: KeyboardEvent) => {
+      if (e.code === "Escape") {
+        onClick();
+      }
+    };
+    window.addEventListener("keydown", handleEscDown);
+    disableBodyScroll();
+    return () => {
+      window.removeEventListener("keydown", handleEscDown);
+      enableBodyScroll();
+    };
+  }, [onClick]);
+
+  function disableBodyScroll() {
+    document.body.style.overflow = "hidden";
+  }
+  function enableBodyScroll() {
+    document.body.style.overflow = "auto";
+  }
+
+  const handleBackdropClick = () => {
+    onClick();
+  };
+
+  return (
+    <>
+      <div className="Overlay" onClick={handleBackdropClick}>
+        <div className="Modal" onClick={(e) => e.stopPropagation()}>
+          <button onClick={handleBackdropClick} type="button">
+            <img src={cross} alt="cross" width={14} />
+          </button>
+          {children}
+        </div>
+      </div>
+    </>
+  );
 };
