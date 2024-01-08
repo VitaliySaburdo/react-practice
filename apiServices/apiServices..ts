ecf5ae53bf085ff997e9b9ef9fdea43087fcf70f
VitaliySaburdo <vsaburdo@gmail.com>
2024-01-08 10:56:30 +0200

    add data

---
+++ apiServices/apiServices..ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'ab57a8d74b0df3fdba80a78e42f32d17';
+
+export const fetchMoviesData = async() => {
+    try {
+        const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}&page=1&language=en`;
+        const response = await axios.get(url);
+        return response.data;
+      } catch (error) {
+        console.log(error);
+      }
+}
