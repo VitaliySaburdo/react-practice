1c06ec25d196d86d5b5c3a22dbc19af28905d916
VitaliySaburdo <vsaburdo@gmail.com>
2024-01-08 19:05:32 +0200

    add

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,3 +1,9 @@
-export const Section = () => {
-  return <></>;
+import { StyledSection } from "./Section.styled";
+
+interface Children {
+  children: React.ReactNode;
+}
+
+export const Section: React.FC<Children> = (props) => {
+  return <StyledSection {...props} />;
 };
